8d26c022cf2577f1b76ef0f3b283bb5a5d8eccb0
BrodyMacfarlane <macfarlanebrody@gmail.com>
2015-12-10 17:31:16 -0700

    betternbetter

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -1,3 +1,33 @@
-$('.x').on('click', function(){
-	$('.messed').css('display', 'none')
+$(document).ready(function(){
+	$('.x').on('click', function(){
+		$('.messed').css('display', 'none')
+	})
+	var timey;
+	var timer = function(){
+		timey = setTimeout(function(){
+			$('.explain').removeClass('delay');
+			timey = setTimeout(function(){
+				$('.smallimg').css('height', '320px')
+				timey = setTimeout(function(){
+					$('.smallimg').css('height', '300px')
+				}, 200)
+			}, 200)
+		}, 1000)
+	}
+	var stopTimey = function(){
+		clearTimeout(timey);
+		$('.explain').addClass('delay');
+	}
+	$('.project-container').mouseenter(function(){
+		$('.project-container').removeClass('blur')
+		$('.bitcurve-title').addClass('bitcurve-title-moved')
+		timer();
+	})
+	$('.project-container').mouseleave(function(){
+		$('.project-container').addClass('blur')
+		$('.bitcurve-title').removeClass('bitcurve-title-moved')
+		stopTimey();
+		console.log('stopped timey');
+	})
+	
 })
